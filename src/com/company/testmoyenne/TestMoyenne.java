e30d1f7560f5e60b055f21e05e1208bbc96c9f94
abd elatif <uabdalatif@gmail.com>
2021-04-10 21:47:58 +0100

    .

--- src/com/company/testmoyenne/TestMoyenne.java
+++ src/com/company/testmoyenne/TestMoyenne.java
@@ -1,5 +1,27 @@
 package com.company.testmoyenne;
 
 public class TestMoyenne {
-    
+    public static void main(String[] args){
+        int j;
+        double y;
+        float k = 2.5F;
+        int N = 5;// N est le nombre max des notes.
+        Lecture L = new Lecture(); //declaration et creation de l'objet L
+        Moyenne M1 = new Moyenne();
+        Moyenne M2 = new Moyenne(k);
+        for (j = 0; j <= N-1; j++){
+            y = L.saisirDouble();
+            M1.ajoutNote(y);
+            M2.ajoutNote(y);
+        }
+
+        double m = M1.calculMoyenne();
+        System.out.println("la Moyenne est : "+ m);
+        System.out.println("la Moyenne est : "+ M1.calculMoyenne() );
+        int A = M1.affichenombreDeNote();
+        System.out.println("le nombre de note : " + A);
+        System.out.println("la Moyenne est : " + M1.affichenombreDeNote());
+
+
+    }
 }
